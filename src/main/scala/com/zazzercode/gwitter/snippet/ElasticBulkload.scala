b0387ddfc7d9fa814ea0ebda3513e9de642e3b3c
Prayag Upadhyay <prayag.upd@gmail.com>
2013-10-23 07:40:58 -0400

    ADD log.json and index it

---
+++ src/main/scala/com/zazzercode/gwitter/snippet/ElasticBulkload.scala
@@ -0,0 +1,21 @@
+package zazzercode
+ 
+import org.elasticsearch.node.NodeBuilder.nodeBuilder
+import scala.io.Source
+/**
+  * curl -XGET http://localhost:9200/logs/log/1
+  */ 
+object ElasticBulkload extends App {
+  val node = nodeBuilder().client(true).node()
+  val client = node.client()
+ 
+  val jsons = Source.fromFile("src/main/resources/log.json").getLines().toList
+  for (i <- 1 to jsons.length) {
+    indexJson(jsons(i - 1), i.toString)
+  }
+ 
+  def indexJson(json: String, id: String) = {
+    client.prepareIndex("logs", "log", id).setSource(json).execute().actionGet()
+  }
+ 
+}
